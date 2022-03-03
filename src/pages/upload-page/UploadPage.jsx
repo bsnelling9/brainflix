bbc98b26f478a3275a3daecc246a33d6a4eddf51
Brodie <brodiesnelling@gmail.com>
2022-03-03 17:03:24 -0500

    form-updates

--- src/pages/upload-page/UploadPage.jsx
+++ src/pages/upload-page/UploadPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component, createRef } from 'react';
+import { Link } from 'react-router-dom';
 import PublishIcon from '../../assets/Icons/publish.svg';
 import Thumbnail from '../../assets/Images/Upload-video-preview.jpg';
 import './UploadPage.scss';
@@ -8,15 +9,22 @@ export default class UploadPage extends Component {
 
     state = {
         title: '',
-        text: ''
+        text: '',
+        link: "#"
     }
     
     isFormValid(){
         if(!this.state.text) {
             this.setState({validate: true})
         }
+        else if (!this.state.title) {
+            this.setState({validateTitle: true})
+        }
+        else {
+            this.setState({validate: false})
+            return true
+        } 
     }
-
 
     handleChange = event => {
         this.setState({
@@ -28,6 +36,7 @@ export default class UploadPage extends Component {
         event.preventDefault();
         if(this.isFormValid()) {
             alert("signed up");
+            window.location.href= "/";
         }else {
             alert("ouuf");
         }
@@ -39,7 +48,7 @@ export default class UploadPage extends Component {
                 <div className='upload__wrapper'>
                     <h2 className='page-title'>Upload Video</h2>
                     <div className='upload__container'>
-                        <form action="post" className='form upload__form' onSubmit={this.handleSubmit}>
+                        <form method="post" className='form upload__form' onSubmit={this.handleSubmit}>
                             <div className='upload__form--container'>
                                 <div className='upload__container--thumbnail'>
                                     <label htmlFor='thumbnail' className='form__label'>video thumbnail</label>
@@ -50,10 +59,9 @@ export default class UploadPage extends Component {
                                 <div className='upload__content'>
                                     <label className='form__label' htmlFor='title'>title your video</label>
                                     <input
-                                        className='form__text upload__form--text'
+                                        className={this.state.validateTitle ? 'error form__text upload__form--text' : 'form__text upload__form--text'}
                                         name='title'
                                         placeholder='Add a title to your video'
-                                        required
                                         onChange={this.handleChange}
                                     />
                                     <label className="form__label" htmlFor="text">add a video description</label>
@@ -61,28 +69,27 @@ export default class UploadPage extends Component {
                                         name="text"
                                         type="text"
                                         placeholder="Add a description to your video"
-                                        
                                         autoComplete='off'
                                         onChange={this.handleChange}
                                     />
                                 </div> 
                             </div>
                             <div className='button__container'>
-                                <button className='cancel__btn btn-not-mobile'>
+                                <a className='cancel__btn btn-not-mobile' href='/'>
                                     cancel
-                                </button>
+                                </a>
                                 <button className="btn upload__btn" type="submit">
                                     <img className='btn__ico' src={PublishIcon} alt='upload icon'/>
                                     <span className='btn__text'>publish</span>
                                 </button> 
                             </div>
-                            <button className='cancel__btn btn-mobile'>
+                            <a className='cancel__btn btn-mobile' href='/'>
                                 cancel
-                            </button>
+                            </a>
                         </form>
                     </div>
                 </div>
-            </section>
+            </section>  
         );
     }
 }
